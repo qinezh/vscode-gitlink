c4082a92c98ee39e32a1eb2a649ffc4cab135fe5
Carlos Pereira <carlos.pereira@outlinksoftware.com>
2017-01-29 18:05:55 +0000

    Update linkInfo.ts
    Ohh sorry :O, i was really high when i wrote that :D, it should be fixed now...

--- src/linkInfo.ts
+++ src/linkInfo.ts
@@ -13,7 +13,7 @@ export class LinkInfo {
     public toLink(): string {
         var startIndex = this.position.start.line + 1;
         var endIndex = this.position.end.line + 1;
-        var siteUrlSource = (${this.siteName} == "bitbucket.org") ? 'src' : 'blob';
+        var siteUrlSource = (this.siteName.split("@")[1] === "bitbucket.org") ? 'src' : 'blob';
         var prefix = `https://${this.siteName}/${this.userName}/${this.repoName}/${siteUrlSource}/${this.branchName}/${this.filePath}`;
         if (startIndex === endIndex) {
             return `${prefix}#L${startIndex}`;
